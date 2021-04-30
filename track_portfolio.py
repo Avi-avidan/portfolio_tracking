827123ea6833d2a2282da5b0c9762db89406d641
Avi Avidan <avi.avidan@logmein.com>
2021-04-30 11:52:45 +0300

    token conflict resolved

--- track_portfolio.py
+++ track_portfolio.py
@@ -1,7 +1,8 @@
 from portfolio import Portfolio, telegramBot
-etoro_p = Portfolio('etoro', 71676.96)
-bh_p = Portfolio('bh', 70000)
+from token_ import tele_token
 
-tele_token = '1685123002:AAHby83yoK0JNrpp2-g11pphnvWs5gVNo1g'
-tbot = telegramBot(tele_token, portfolios=[etoro_p, bh_p])
-tbot.loop_for_telegram()
+while(True):
+    etoro_p = Portfolio('etoro', 71676.96) #, portfolio_csv='csvs/etoro_portfolio_2021-04-29.csv', pnl_csv='csvs/etoro_pnl_2021-04-29.csv')
+    bh_p = Portfolio('bh', 70000) #, portfolio_csv='csvs/bh_portfolio_2021-04-29.csv', pnl_csv='csvs/bh_pnl_2021-04-29.csv')
+    tbot = telegramBot(tele_token, portfolios=[etoro_p, bh_p])
+    tbot.loop_for_telegram()
